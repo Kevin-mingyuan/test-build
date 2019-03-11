1fa28d506306cba3fd2bf963fbe8cfac2fc68a00
liuming <1401389531@qq.com>
2019-03-11 18:14:20 +0800

    3-11

--- project/src/main.js
+++ project/src/main.js
@@ -4,9 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from "./store"
-import animate from "animate.css" 
-Vue.use(animate); //引入animate动画
+
 Vue.config.productionTip = false
+
+import animated from 'animate.css' // npm install animate.css --save安装，再引入
+Vue.use(animated)
+
+import MintUI from 'mint-ui'; //引入 mint-ui
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI);
+
+import Vant from 'vant';  //引入vant
+import 'vant/lib/index.css';
+Vue.use(Vant);
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'  //引入swiper            
+// require styles
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 /* eslint-disable no-new */
 new Vue({
